a6b3366ad6358e2dd6e3f8b901b2af34e546da5a
Marko <m.nikolic.sm@gmail.com>
2026-04-06 21:41:27 +0200

    Finish 4.1-4.2-blog

---
+++ part4/4.1-4.2-blog/app.js
@@ -0,0 +1,24 @@
+const config = require("./utils/config");
+const express = require('express');
+const mongoose = require('mongoose');
+const blogRouter = require("./controllers/blog");
+
+const app = express();
+
+
+
+const mongoUrl = config.MONGODB_URI;
+mongoose.connect(mongoUrl, { family: 4 })
+    .then(() => {
+        console.log("connected to mongodb");
+    })
+    .catch(error => {
+        console.log("error connecting to mongodb: " + error.message);
+    })
+
+
+app.use(express.json());
+
+app.use("/api/blogs", blogRouter);
+
+module.exports = app;
